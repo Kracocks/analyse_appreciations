e17b61861c25f6067dccbb254921746032e2e996
RomainVoivenel <voivenelromain@gmail.com>
2025-06-19 11:22:23 +0200

    supprésion input choix variables

--- src/static/on_parametre_change.js
+++ src/static/on_parametre_change.js
@@ -1,13 +1,4 @@
 document
-    .getElementById("variables_choice")
-    .addEventListener("change", (event) => {
-        console.log(event)
-        document
-            .getElementById("parametre_form")
-            .submit()
-    })
-
-document
     .getElementById("modeles_choice")
     .addEventListener("change", (event) => {
         console.log(event)
